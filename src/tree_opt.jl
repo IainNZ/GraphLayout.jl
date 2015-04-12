5d7000e9a15a6d106401b27220feb91832d8cd95
Iain Dunning <iaindunning@gmail.com>
2015-04-12 14:09:21 -0400

    Cleanup for opt ordering

--- src/tree_opt.jl
+++ src/tree_opt.jl
@@ -10,10 +10,21 @@ using JuMP
     Given a layer assignment, decide a permutation for each layer
     that minimizes edge crossings using integer programming.
 
+    Based on the IP described in
+      M. Junger, E. K. Lee, P. Mutzel, and T. Odenthal.
+      A polyhedral approach to the multi-layer crossing minimization problem.
+      In G. Di Battista, editor, Graph Drawing: 5th International Symposium,
+      GD ’97, volume 1353 of Lecture Notes in Computer Science, pages 13–24,
+      Rome, Italy, September 1997. Springer-Verlag.
+
+
     Arguments:
     adj_list        Directed graph in adjacency list format
     layers          Assignment of vertices
-    layer_verts     Dictionary of layer => vertices
+    layer_verts     Dictionary of layer => vertices (initial perm.)
+
+    Returns:
+    new_layer_verts An improved dictionary of layer => vertices (opt. perm.)
 """ ->
 function _ordering_ip{T}(adj_list::AdjList{T}, layers, layer_verts)
     num_layers = maximum(layers)
@@ -21,30 +32,34 @@ function _ordering_ip{T}(adj_list::AdjList{T}, layers, layer_verts)
     m = Model()
 
     # Define crossing binary variables
-    @defVar(m, c[L=1:num_layers,
-                 i=layer_verts[L],
-                 j=adj_list[i],
-                 k=layer_verts[L],
-                 l=adj_list[k]], Bin)
-
-    # Define permutation variables for each layer
-    # We'll define for both (i,j) and (j,i), and they are consistent
-    # by adding constraints. Presolve in the IP solver will simplify
-    # the problem for us by removing one of the variables.
-    @defVar(m, x[L=1:num_layers,
-                 i=layer_verts[L],
-                 j=layer_verts[L]], Bin)
-    for L in 1:num_layers
-        for (p,i) in enumerate(layer_verts[L])
-            for (q,j) in enumerate(layer_verts[L])
-                p <= q && continue
-                @addConstraint(m, x[L,i,j] == 1 - x[L,j,i])
-            end
-        end
-    end
+    @defVar(m, c[L=1:num_layers,        # for each layer
+                 i=layer_verts[L],      # for each vertex in this layer
+                 j=adj_list[i],         # and vertex in the next layer
+                 k=layer_verts[L],      # for each vertex in this layer
+                 l=adj_list[k]], Bin)   # and vertex in the next layer
 
     # Objective: minimize crossings
     @setObjective(m, Min, sum(c))
+
+    # Define permutation variables for each layer
+    # We'll define for both (i,j) and (j,i), and ensure they consistency
+    # by adding constraints. Presolve in the IP solver will simplify
+    # the problem for us by removing one of the variables.
+    @defVar(m, x[L=1:num_layers, i=layer_verts[L], j=layer_verts[L]], Bin)
+    for L in 1:num_layers
+        for i in layer_verts[L]
+            for j in layer_verts[L]
+                j <= i && continue  # Don't double-add
+                # Ensure x[i,j] and x[j,i] are consistent
+                @addConstraint(m, x[L,i,j] == 1 - x[L,j,i])
+                # And ensure that triples are consistent
+                for k in layer_verts[L]
+                    k <= j && continue
+                    @addConstraint(m, 0 <= x[L,i,j] + x[L,j,k] - x[L,i,k] <= 1)
+                end
+            end
+        end
+    end
 
     # Link permutations to crossings
     for L in 1:num_layers-1
@@ -55,24 +70,11 @@ function _ordering_ip{T}(adj_list::AdjList{T}, layers, layer_verts)
             for k in layer_verts[L]
             k == i && continue  # Can't cross if starting from same vertex!
             for l in adj_list[k]
-                    @addConstraint(m, -c[L,i,j,k,l] <= x[L+1,j,l] - x[L,i,k])
-                    @addConstraint(m,  c[L,i,j,k,l] >= x[L+1,j,l] - x[L,i,k])
+                @addConstraint(m, -c[L,i,j,k,l] <= x[L+1,j,l] - x[L,i,k])
+                @addConstraint(m,  c[L,i,j,k,l] >= x[L+1,j,l] - x[L,i,k])
             end
             end
         end
-        end
-    end
-
-    # Enforce consistency in permutations
-    for L in 1:num_layers
-        for i in layer_verts[L]
-            for j in layer_verts[L]
-                j <= i && continue
-                for k in layer_verts[L]
-                    k <= j && continue
-                    @addConstraint(m, 0 <= x[L,i,j] + x[L,j,k] - x[L,i,k] <= 1)
-                end
-            end
         end
     end
 
@@ -84,6 +86,9 @@ function _ordering_ip{T}(adj_list::AdjList{T}, layers, layer_verts)
     new_layer_verts = [L => Int[] for L in 1:num_layers]
     for L in 1:num_layers
         old_perm = layer_verts[L]
+        # For each vertex, count the number of times it is "in front"
+        # of the other vertices. The higher this number, the earlier
+        # the vertex appears in the layer.
         scores = zeros(length(old_perm))
         for (p,i) in enumerate(old_perm)
             for j in old_perm
